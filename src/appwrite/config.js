377cefc863f893711a43973a9d3eb780735c5d59
Virendr-Balasaheb-Desai <desaivirendra959@gmail.com>
2025-08-10 15:52:56 +0530

    Database and File upload Service

---
+++ src/appwrite/config.js
@@ -0,0 +1,143 @@
+import conf from "../conf/conf"
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service{
+    client = new Client()
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    slug,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Create Post Error :",error);
+            return null;
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Update Post Error :",error);
+            return null;
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Delete Post Error :",error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            );
+
+        } catch (error) {
+            console.log("Get Post Error :",error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal('status','active')]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            );
+
+        } catch (error) {
+            console.log("Get Active Posts Error :",error);
+            return false;
+        }
+    }
+
+    // File upload service 
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            );
+
+        } catch (error) {
+            console.log("Upload File Error :",error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            );
+
+        } catch (error) {
+            console.log("Delete File Error :",error);
+            return false;
+        }
+    }
+
+    async getFilePreview(fileId){
+        try {
+            return this.bucket.getFilePreview(
+                conf.appwriteBucketId,
+                fileId
+            );
+
+        } catch (error) {
+            console.log("File Preview Error :",error);
+            return false;
+        }
+    }
+}
+
+const service = new Service();
+export default service;
