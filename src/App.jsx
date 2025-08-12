75ea8890aa6b8f71072c79491af3add1db242f59
Virendr-Balasaheb-Desai <desaivirendra959@gmail.com>
2025-08-12 21:19:38 +0530

    Redux setup, other initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
 import './App.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth"
+import {login,logout} from './store/authSlicer'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
-  return (
-    <>
-      <h1>HOME</h1>
-    </>
-  )
+	const [loading, setLoading] = useState(true)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		authService.getCurrentUser()
+		.then((userData) => {
+			if(userData){
+				dispatch(login({userData}))
+			}
+			else{
+				dispatch(logout())
+			}
+		})
+		.catch((error) => {
+			console.log("Auth useEffect Error: " ,error);
+			
+		})
+		.finally(()=>{
+			setLoading(false);
+		})
+	}, [])
+
+
+	return !loading ? 
+		<div className='min-h-screen w-full bg-gray-600 flex flex-wrap content-between'>
+			<div className='w-full block'>
+				<Header/>
+				<main>
+					<Outlet/>
+				</main>
+				<Footer/>
+			</div>
+		</div> 
+		:
+		<div>Loading</div>
 }
 
 export default App
